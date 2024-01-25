add36b7911fe639b0ae34ac88b77cdc5bfbdb6c7
y2456y16 <y2456y16@gmail.com>
2024-01-25 17:49:44 +0900

    <refactor> StatSO 코드 수정
    전체적인 코드 수정중1

--- Assets/Scripts/CharacterStatHandler.cs
+++ Assets/Scripts/CharacterStatHandler.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class CharacterStatHandler : MonoBehaviour
 {
+    [SerializeField]
+    private CharacterStat CharacterbaseStats;
+
+    public CharacterStat CurrentStats { get; private set; }
+
+    public List<CharacterStat> CstatsModifier = new List<CharacterStat>();
+
+    private void Awake()
+    {
+        UpdateCharacterStats();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +25,16 @@ public class CharacterStatHandler : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void UpdateCharacterStats()
+    {
+        CharacterStatSO CharacterSO = null;
+        if (CharacterbaseStats.statInfo != null)
+        {
+            CharacterSO = Instantiate(CharacterbaseStats.statInfo);
+        }
+
+        CurrentStats = new CharacterStat { statInfo = CharacterSO };
     }
 }
