aceabdfd5d70533dbc91a70d82d5e857b9c84038
정원우 <54103715+klik075@users.noreply.github.com>
2024-01-26 00:31:14 +0900

    fix : 스텟 컨트롤러 변경 해야할 부분 있음

--- Assets/Scripts/CharacterStatHandler.cs
+++ Assets/Scripts/CharacterStatHandler.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 
 public class CharacterStatHandler : MonoBehaviour
 {
-    [SerializeField]
-    private CharacterStat CharacterbaseStats;
-
+    [SerializeField] private CharacterStat CharacterbaseStats;
     public CharacterStat CurrentStats { get; private set; }
 
     public List<CharacterStat> CstatsModifier = new List<CharacterStat>();
@@ -18,18 +16,18 @@ public class CharacterStatHandler : MonoBehaviour
     }
     // Start is called before the first frame update
 
-    private void UpdateCharacterStats() //수정 고려
-    {
-        AttackSO attackSO = null;
-        if (baseStats.attackSO != null)
-        {
-            attackSO = Instantiate(baseStats.attackSO);
-        }
-        currentStats = new CharacterStat { attackSO = attackSO };
-        currentStats.maxHp = baseStats.maxHp;
-        currentStats.speed = baseStats.speed;
-        //������ ���ݵ鵵 �����ؾ� ��
-    }
+    //private void UpdateCharacterStats() //수정 고려
+    //{
+    //    AttackSO attackSO = null;
+    //    if (baseStats.attackSO != null)
+    //    {
+    //        attackSO = Instantiate(baseStats.attackSO);
+    //    }
+    //    currentStats = new CharacterStat { attackSO = attackSO };
+    //    currentStats.maxHp = baseStats.maxHp;
+    //    currentStats.speed = baseStats.speed;
+    //    //������ ���ݵ鵵 �����ؾ� ��
+    //}
 
     private void UpdateCharacterStats()
     {
