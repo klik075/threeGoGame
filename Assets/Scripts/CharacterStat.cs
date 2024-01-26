0fdc0ffdaf530fe8216f15bf82bab7bdb4e09f4b
y2456y16 <y2456y16@gmail.com>
2024-01-26 10:43:55 +0900

    <refactor> 데이터 삭제

--- Assets/Scripts/CharacterStat.cs
+++ Assets/Scripts/CharacterStat.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 public enum CharacterType
 {
     Player1,
-    Player2
+    Player2,
+    Monster1,
+    Monster2
+}
+
+public enum StatsChangeType
+{
+    Add,
+    Multiple,
+    Override
 }
 
 public class CharacterStat
 {
-    public CharacterType type;
-    public float maxHp;
-    public float speed;
-    public CharacterStatSO statInfo;
+    public CharacterType Charactertype;
+    public StatsChangeType statsChangeType;
+    public string name;
+    public string info;
+    public int lv;
+    public float exp;
+    public float fullExp;
+    public float hp;
+    public float attack;
+    [Range(1, 100)] public float maxHp;
+    [Range(1f, 20f)] public float speed;
+    
+
+    public AttackSO attackSO;
 }
