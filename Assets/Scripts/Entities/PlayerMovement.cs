e0f37c0fac6f0289a9c57e939ab4db70a6a1d7fd
정원우 <54103715+klik075@users.noreply.github.com>
2024-01-26 17:14:35 +0900

    feat : 스텟 핸들러 내용 추가 , 이동 넉백 추가

--- Assets/Scripts/Entities/PlayerMovement.cs
+++ Assets/Scripts/Entities/PlayerMovement.cs
@@ -8,6 +8,8 @@ public class PlayerMovement : MonoBehaviour
     private CharacterStatHandler _stats;
     private Vector2 _movementDirection = Vector2.zero;
     private Rigidbody2D _rigidbody;
+    private Vector2 _knockback = Vector2.zero;
+    private float knockbackDuration = 0.0f;
 
     private void Awake()
     {
@@ -22,27 +24,31 @@ public class PlayerMovement : MonoBehaviour
         _controller.OnMoveEvent += Move;
     }
 
-    protected virtual void FixedUpdate()
+    private void FixedUpdate()
     {
         ApplyMovement(_movementDirection);
-        //if (knockbackDuration > 0.0f)
-        //{
-        //    knockbackDuration -= Time.fixedDeltaTime;
-        //}
+        if (knockbackDuration > 0.0f)
+        {
+            knockbackDuration -= Time.fixedDeltaTime;
+        }
     }
 
     private void Move(Vector2 direction)
     {
         _movementDirection = direction;
     }
-
+    public void ApplyKnockback(Transform other, float power, float duration)
+    {
+        knockbackDuration = duration;
+        _knockback = -(other.position - transform.position).normalized * power; 
+    }
     private void ApplyMovement(Vector2 direction)
     {
         direction = direction * _stats.CurrentStats.speed;
-        //if (knockbackDuration > 0.0f)
-        //{
-        //    direction += _knockback;
-        //}
+        if (knockbackDuration > 0.0f)
+        {
+            direction += _knockback;
+        }
         _rigidbody.velocity = direction;
     }
 }
