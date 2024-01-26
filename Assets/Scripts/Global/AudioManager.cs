bd7345c2b78b429f2d96494ddf44a49ee49ed3ab
Amberjack10 <sanghwasong10@gmail.com>
2024-01-26 20:26:59 +0900

    <feat> : audioManager 코드

--- Assets/Scripts/Global/AudioManager.cs
+++ Assets/Scripts/Global/AudioManager.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager instance;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip bgMusic;     // πË∞Ê¿Ωæ«
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource.clip = bgMusic;
+        audioSource.Play();
     }
 
     // Update is called once per frame
