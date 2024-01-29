474baf9c215e3f842c323c0783f211899ee9e557
Amberjack10 <sanghwasong10@gmail.com>
2024-01-29 14:18:04 +0900

    <feat> : BGM 사운드 조절 기능 추가

---
+++ Assets/Scripts/Controllers/AudioMixController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioMixController : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider BGMSlider;
+    [SerializeField] private Toggle BGMMute;
+
+
+    private void Awake()
+    {
+        BGMSlider.onValueChanged.AddListener(SetBGMVolume);
+        BGMMute.onValueChanged.AddListener(SetBGMMute);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BGMSlider.value = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetBGMVolume(float volume)
+    {
+        audioMixer.SetFloat("BGM", Mathf.Log10(volume) * 20);
+    }
+
+    private void SetBGMMute(bool mute)
+    {
+        AudioListener.volume = (mute ? 0 : 1);
+    }
+}
